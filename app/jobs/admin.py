2411aeaf9e5ba849de48691974c5be561d45a38e
Marius Räsener <marius@raesener.de>
2020-02-10 11:09:18 +0100

    WIP, good progress actually, next step is to integrate f.e. django-nested-admin

--- app/jobs/admin.py
+++ app/jobs/admin.py
@@ -1,21 +1,99 @@
 from django.contrib import admin
-from .models import Job, Tasklevel0
+from .models import Job, Chapter, Section, Paragraph, Title, Task, LineItem
 
 
-class Tasklevel0Inline(admin.TabularInline):
-    model = Tasklevel0
+class ChapterInline(admin.TabularInline):
+    model = Chapter
+    show_change_link = True
+
+
+class SectionInline(admin.TabularInline):
+    model = Section
+    show_change_link = True
+
+
+class ParagraphInline(admin.TabularInline):
+    model = Paragraph
+    show_change_link = True
+
+
+class TitleInline(admin.TabularInline):
+    model = Title
+    show_change_link = True
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    show_change_link = True
+
+
+class LineItemInline(admin.TabularInline):
+    model = LineItem
+    show_change_link = True
 
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     model = Job
 
+    def get_inlines(self, request, obj):
+        if obj.tasks.count() == 0:
+            return [LineItemInline]
+        if obj.titles.count() == 0:
+            return [TaskInline]
+        if obj.paragraphs.count() == 0:
+            return [TitleInline]
+        if obj.sections.count() == 0:
+            return [ParagraphInline]
+        return [ChapterInline]
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    model = Chapter
+
     inlines = [
-        Tasklevel0Inline,
+        SectionInline,
     ]
 
 
-@admin.register(Tasklevel0)
-class Tasklevel0Admin(admin.ModelAdmin):
-    model = Tasklevel0
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    model = Section
+
+    inlines = [
+        ParagraphInline,
+    ]
+
+
+@admin.register(Paragraph)
+class ParagraphAdmin(admin.ModelAdmin):
+    model = Paragraph
+
+    inlines = [
+        TitleInline,
+    ]
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    model = Title
+
+    inlines = [
+        TaskInline,
+    ]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+
+    inlines = [
+        LineItemInline,
+    ]
+
+
+@admin.register(LineItem)
+class LineItemAdmin(admin.ModelAdmin):
+    model = LineItem
 
--- app/jobs/models.py
+++ app/jobs/models.py
@@ -16,13 +16,71 @@ class Job(models.Model):
         ordering = ["order"]
 
 
-class Tasklevel0(models.Model):
+class Chapter(models.Model):
+    order = models.IntegerField("Reihenfolge")
+    name = models.CharField("Bezeichnung", max_length=50)
+
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="chapters")
+
+    class Meta:
+        ordering = ["order"]
+
+
+class Section(models.Model):
+    order = models.IntegerField("Reihenfolge")
+    name = models.CharField("Bezeichnung", max_length=50)
+
+    group = models.ForeignKey(Chapter, on_delete=models.PROTECT)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="sections")
+
+    class Meta:
+        ordering = ["order"]
+
+
+class Paragraph(models.Model):
+    order = models.IntegerField("Reihenfolge")
+    name = models.CharField("Bezeichnung", max_length=50)
+
+    group = models.ForeignKey(Section, on_delete=models.PROTECT)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="paragraphs")
+
+    class Meta:
+        ordering = ["order"]
+
+
+class Title(models.Model):
+    order = models.IntegerField("Reihenfolge")
+    name = models.CharField("Bezeichnung", max_length=50)
+
+    group = models.ForeignKey(Paragraph, on_delete=models.PROTECT)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="titles")
+
+    class Meta:
+        ordering = ["order"]
+
+
+class Task(models.Model):
+    order = models.IntegerField("Reihenfolge")
+    index = models.IntegerField("Index")
+    name = models.CharField("Bezeichnung", max_length=255)
+    description = models.TextField("Beschreibung")
+    unit = models.CharField("Einheit", max_length=10)
+
+    group = models.ForeignKey(Title, on_delete=models.PROTECT)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="tasks")
+
+    class Meta:
+        ordering = ["order"]
+
+
+class LineItem(models.Model):
     order = models.IntegerField("Reihenfolge")
     name = models.CharField("Bezeichnung", max_length=255)
-    unit = models.CharField("Einheit", max_length=3)
+    unit = models.CharField("Einheit", max_length=10)
     price = MoneyField(max_digits=17, decimal_places=2, default_currency="EUR")
 
-    job = models.ForeignKey(Job, on_delete=models.PROTECT)
+    task = models.ForeignKey(Task, on_delete=models.PROTECT)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT, related_name="lineitems")
 
     class Meta:
         ordering = ["order"]
